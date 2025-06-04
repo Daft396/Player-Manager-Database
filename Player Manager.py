31dd381bf70d02fcbf7557758f25a75030c37e19
Pirran Hyde-Thomas <81690719+Daft396@users.noreply.github.com>
2025-06-04 10:34:59 +0100

    Added stuff

--- Player Manager.py
+++ Player Manager.py
@@ -12,5 +12,36 @@ Level INTEGER NOT NULL);'''
 cursor.execute(Create)
 DB.commit()
 
+def display_interface():
+    print("Welcome to the Game Character Manager")
+    print("\n")
+    print("1) Add Character")
+    print("2) View all Characters")
+    print("3) Update Character")
+    print("4) Delete Character")
+    print("5) Exit")
+    print("\n")
+    choice = int(input("Enter Your Choice: "))
+    while choice != 5:
+        if choice == 1:
+            addchar()
+        elif choice== 2:
+            viewchars()
+        elif choice== 3:
+            upchar()
+        elif choice == 4:
+            delchar()
+        elif choice == 5:
+            print("Thanks for using our program!")
+            break
+def addchar():
+    pass
 
+def viewchars():
+    pass
 
+def upchar():
+    pass
+
+def delchar():
+    pass
